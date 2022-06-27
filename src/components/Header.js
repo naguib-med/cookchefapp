9bc541bebb8467c6e90219bebdb90486d9f2c3be
Naguib <naguib.mohamed1999@gmail.com>
2022-06-28 00:36:53 +0200

    added header menu responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,33 @@
 import styles from "./Header.module.scss";
 import cookchef from "../assets/images/cookchef.png";
+import { useState } from "react";
+import HeaderMenu from "./HeaderMenu";
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <header className={`${styles.header} d-flex flex-row align-items-center`}>
-      <i class="fa-solid fa-bars mr-15"></i>
       <div className="flex-fill">
         <img src={cookchef} alt="logo cookchef" />
       </div>
-      <ul>
+      <ul className={styles.headerList}>
         <button className="mr-5 btn btn-reverse-primary">
-          <i class="fa-solid fa-basket-shopping mr-5"></i>
-          <span>panier</span>
+          <i class="fa-solid fa-heart mr-5"></i>
+          <span>Wishlist</span>
         </button>
         <button className="btn btn-primary">connexion</button>
       </ul>
+      <i
+        onClick={() => setShowMenu(true)}
+        class={`fa-solid fa-bars ${styles.headerXs}`}
+      ></i>
+      {showMenu && (
+        <>
+          <div onClick={() => setShowMenu(false)} className="calc"></div>
+          <HeaderMenu />
+        </>
+      )}
     </header>
   );
 }
