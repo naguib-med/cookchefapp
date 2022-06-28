9880aa604558d0b45061b7b40d032e352064955d
Naguib <naguib.mohamed1999@gmail.com>
2022-06-28 17:04:23 +0200

    search bar added

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,17 +1,37 @@
 import styles from "./Content.module.scss";
 import Recipe from "./Recipe";
 import { data } from "../data/recipes";
+import { useState } from "react";
 
 function Content() {
   const recipes = data;
+  const [filter, setFilter] = useState("");
+
+  function handleInput(e) {
+    const filter = e.target.value;
+    setFilter(filter.trim().toLowerCase());
+  }
   return (
     <div className="flex-fill container">
       <h1 className="my-30">Décrouvrez nos nouvelles recettes</h1>
-      <div className={`card p-20 ${styles.contentCard}`}>
+      <div className={`card d-flex flex-column p-20 ${styles.contentCard}`}>
+        <div
+          className={`d-flex flex-row justify-content-center align-items-center my-30 ${styles.searchBar}`}
+        >
+          <i class="fa-solid fa-magnifying-glass mr-15"></i>
+          <input
+            onInput={handleInput}
+            className="flex-fill "
+            type="text"
+            placeholder="Rechercher"
+          />
+        </div>
         <div className={styles.grid}>
-          {recipes.map((r) => (
-            <Recipe title={r.title} image={r.image} />
-          ))}
+          {recipes
+            .filter((r) => r.title.toLowerCase().startsWith(filter))
+            .map((r) => (
+              <Recipe title={r.title} image={r.image} />
+            ))}
         </div>
       </div>
     </div>
